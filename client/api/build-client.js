0971f8467ca0535be810e5e4e9473531e2f313f9
Brandon Shearin <brandon.shearin@ibm.com>
2021-02-23 21:32:44 -0500

    ingress patch

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,8 +4,8 @@ const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     // we are on the server
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://squawktherapy.com",
+      // "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
       headers: req.headers,
     });
   } else {
