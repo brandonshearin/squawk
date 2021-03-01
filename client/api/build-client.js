f097c76e6266296489e9adf9b116baf8814ba908
Brandon Shearin <brandon.shearin@ibm.com>
2021-03-01 16:27:11 -0500

    next prod image

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,12 +1,17 @@
 import axios from "axios";
 
+const baseUrl =
+  process.env.BASE_URL ||
+  "http://squawktherapy.com";
+const host = process.env.HOST || "www.squawktherapy.com";
+
 const buildClient = ({ req = { headers: {} } }) => {
   // undefined should be a STRING!!!
   if (typeof window === "undefined") {
     // we are on the server
     return axios.create({
-      baseURL: process.env.BASE_URL,
-      headers: { ...req.headers, Host: process.env.HOST },
+      baseURL: baseUrl,
+      headers: { ...req.headers, Host: host },
     });
   } else {
     // we are on the browser
