b0ee51f39abf2894bd9aafca34971609d6b7278b
Brandon Shearin <brandon.shearin@ibm.com>
2021-03-24 11:48:51 -0400

    rename some stuff

--- comments/src/events/publishers/comment-created-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Publisher, Subjects, CommentCreatedEvent } from "@bscommon/common";
-
-export class CommentCreatedPublisher extends Publisher<CommentCreatedEvent> {
-  subject: Subjects.CommentCreated = Subjects.CommentCreated;
-}
--- comments/src/events/publishers/comment-deleted-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Publisher, Subjects, CommentDeletedEvent } from "@bscommon/common";
-
-export class CommentDeletedPublisher extends Publisher<CommentDeletedEvent> {
-  subject: Subjects.CommentDeleted = Subjects.CommentDeleted;
-}
